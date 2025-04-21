b4a60e5e150cea81b9e64f0228a183114e73273c
Marwenrb <rbmarwenrb@gmail.com>
2025-04-21 10:34:47 +0200

    Enhance footer UI: Fix developer credit to single line, improve heart animation, make brand logos more visible

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,12 +1,20 @@
+import React from 'react'
 import cn from 'clsx'
 import { Button } from 'components/button'
 import { Link } from 'components/link'
-import dynamic from 'next/dynamic'
+import { FiMail, FiPhone } from 'react-icons/fi'
+import { FaTwitter, FaLinkedin, FaGlobe, FaHeart } from 'react-icons/fa'
 import s from './footer.module.scss'
+import { BrandMarquee } from './components/BrandMarquee'
 
 export const Footer = () => {
   return (
-    <footer className={cn('theme-light', s.footer)}>
+    <footer className={cn('theme-dark', s.footer)}>
+      <div className={s.partners}>
+        <h2 className={s.partnersTitle}>Our Strategic Partners</h2>
+        <BrandMarquee />
+      </div>
+      
       <div className={cn(s.top, 'layout-grid hide-on-mobile')}>
         <p className={cn(s['first-line'], 'h1')}>
           Necib Nexus <br />
@@ -23,6 +31,7 @@ export const Footer = () => {
           Let's connect
         </Button>
       </div>
+      
       <div className={cn(s.top, 'layout-block hide-on-desktop')}>
         <p className={cn(s['first-line'], 'h1')}>
           Necib Nexus <br />
@@ -30,37 +39,51 @@ export const Footer = () => {
           <br /> Ready to create <br /> something extraordinary
         </p>
       </div>
-      <div className={s.bottom}>
-        <div className={s.links}>
-          <Link
-            className={cn(s.link, 'p-xs')}
-            href="https://twitter.com/necibnexus"
-          >
-            Twitter
-          </Link>
-          <Link
-            className={cn(s.link, 'p-xs')}
-            href="https://linkedin.com/company/necibnexus"
-          >
-            LinkedIn
-          </Link>
-          <Link
-            className={cn(s.link, 'p-xs')}
-            href="https://necibnexus.com/"
-          >
-            Website
-          </Link>
+      
+      <div className={s.mainFooter}>
+        <div className={s.contactInfo}>
+          <div className={s.contactItem}>
+            <FiMail className={s.contactIcon} />
+            <a href="mailto:contact@necibnexus.com" className={s.contactText}>contact@necibnexus.com</a>
+          </div>
+          <div className={s.contactItem}>
+            <FiPhone className={s.contactIcon} />
+            <a href="tel:+213079696895" className={s.contactText}>+213 07 96 96 98 95</a>
+          </div>
         </div>
-        <p className={cn('p-xs', s.tm)}>
-          <span>©</span> {new Date().getFullYear()} Necib Nexus
-        </p>
-        <Button
-          className={cn(s.cta, 'hide-on-desktop')}
-          arrow
-          href="/contact"
-        >
-          Let's connect
-        </Button>
+        
+        <div className={s.socialLinks}>
+          <a href="https://twitter.com/necibnexus" target="_blank" rel="noreferrer" className={s.socialIcon}>
+            <FaTwitter />
+          </a>
+          <a href="https://linkedin.com/company/necibnexus" target="_blank" rel="noreferrer" className={s.socialIcon}>
+            <FaLinkedin />
+          </a>
+          <a href="https://necibnexus.com" target="_blank" rel="noreferrer" className={s.socialIcon}>
+            <FaGlobe />
+          </a>
+        </div>
+      </div>
+      
+      <div className={s.bottom}>
+        <div className={s.copyright}>
+          <p className={cn('p-xs', s.tm)}>
+            <span>©</span> {new Date().getFullYear()} Necib Nexus
+          </p>
+        </div>
+        
+        <div className={s.developerCredit}>
+          <div className={s.devCreditInline}>
+            <span className={s.madeWithText}>Made with</span>
+            <div className={s.heartContainer}>
+              <FaHeart className={s.heartAnimation} />
+            </div>
+            <span className={s.byText}>by</span>
+            <a href="https://marwenrabai.strikingly.com" target="_blank" rel="noreferrer" className={s.devCreditLink}>
+              <span className={s.devName}>Marwen Rabai</span>
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
   )
